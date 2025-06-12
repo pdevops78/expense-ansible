40a45fdfc688aa27da3dc83b1fb2881c4e092a7e
Pavani <vemasani.pavani@gmail.com>
2025-06-12 23:08:16 +0530

    vault secrets

--- ansible.sh
+++ ansible.sh
@@ -1,16 +1,16 @@
  pip install hvac
+ rm -rf *.json
  component=$1
  env=$2
  name=$3
  ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
  ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e @secrets.json -e @app.json
+
  if [ $? -ne 0 ]; then
    echo "not already installed"
    pip install hvac
-
  else
    echo "installed"
-
   fi
 
 
