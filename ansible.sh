4d6cbb7c51561f94ce9bcff396c7bf66bf9e7611
Pavani <vemasani.pavani@gmail.com>
2025-06-12 22:32:18 +0530

    vault secrets

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
  pip install hvac
 component=$1
 env=$2
-ansible-playbook getsecrets.yml -e component_name=$component -e env=$env
+ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
 ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e @secrets.json -e @app.json
 
