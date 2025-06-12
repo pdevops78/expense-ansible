b97c3bea100b4366c7495e1409404a92f6d5e832
Pavani <vemasani.pavani@gmail.com>
2025-06-12 22:10:39 +0530

    vault secrets

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 env=$2
 #ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321
-
+ansible-playbook getsecrets.yml
 ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e @secrets.json
 
