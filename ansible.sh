3d5f287e5b6272c10bebee80e0a25c52c632b9bf
Pavani <vemasani.pavani@gmail.com>
2025-06-14 14:28:14 +0530

    newrelic

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
- pip install hvac
+# pip install hvac
  if [ $? -ne 0 ]; then
     echo "not already installed"
     pip install hvac
@@ -8,8 +8,11 @@
  component=$1
  env=$2
  name=$3
- ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
- ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+# ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
+# ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+
+  ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
+  ansible-playbook -i 172.31.23.178, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
  rm -rf app.json secrets.json
 
 
