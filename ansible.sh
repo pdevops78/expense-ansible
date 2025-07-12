98e7b19a02bf93af20f5a5a1700fa5733639e5e3
Your Name <vemasani.pavani@gmail.com>
2025-07-12 12:16:06 +0530

    release archive

--- ansible.sh
+++ ansible.sh
@@ -9,8 +9,6 @@
  component=$1
  env=$2
  name=$3
-# ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
-# ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
   ansible-playbook getsecrets.yml -e component_name=$component -e env=$env -e project_name=$name
   ansible-playbook -i $component-$env.pdevops78.online, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json'
