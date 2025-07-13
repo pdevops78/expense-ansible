ae30ba1393b0fe36547a06113a855c37c443a965
Your Name <vemasani.pavani@gmail.com>
2025-07-14 03:46:35 +0530

    github actions

--- ansible.sh
+++ ansible.sh
@@ -17,7 +17,7 @@
 aws ec2 describe-instances --filters Name=tag:Name,Values=$component-$env" "Name=instance-state-name,Values=running --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text >inv
 
 ansible-playbook -i inv -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
- rm -rf *.json
+ rm -rf ~/*.json
 
 
 
